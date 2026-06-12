523436e4ff48167fdbaa53586bd4d51a1c00976b
Tattianerl <tattiane85@hotmail.com>
2026-06-12 10:00:53 -0300

    chore: prepara aplicação para deploy

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,8 @@ app.get("/", (request, response) => {
   });
 });
 
-app.listen(3333, () => {
-  console.log("Servidor rodando na porta 3333");
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
