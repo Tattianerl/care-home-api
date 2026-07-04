ee5566757c7d4ef63f5865209c96e2410c237b59
Tattianerl <tattiane85@hotmail.com>
2026-07-03 21:21:46 -0300

    debug download de documentos

--- src/controllers/DownloadPatientDocumentController.ts
+++ src/controllers/DownloadPatientDocumentController.ts
@@ -7,11 +7,15 @@ export class DownloadPatientDocumentController {
   async handle(request: Request, response: Response) {
     const documentId = request.params.id as string;
 
+    console.log("ID recebido:", documentId);
+
     const document = await prisma.patientDocument.findUnique({
       where: {
         id: documentId,
       },
     });
+
+    console.log("Documento encontrado:", document);
 
     if (!document) {
       return response.status(404).json({
@@ -25,6 +29,9 @@ export class DownloadPatientDocumentController {
       document.arquivo
     );
 
+    console.log("Caminho do arquivo:", filePath);
+    console.log("Arquivo existe?", fs.existsSync(filePath));
+
     if (!fs.existsSync(filePath)) {
       return response.status(404).json({
         error: "Arquivo físico não encontrado",
@@ -32,7 +39,6 @@ export class DownloadPatientDocumentController {
     }
 
     const extension = path.extname(document.arquivo);
-
     const downloadName = `${document.nome}${extension}`;
 
     return response.download(filePath, downloadName);
